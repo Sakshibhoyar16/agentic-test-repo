2854e9f928033d8a6417be0211ba010ca4c1ca41
Sakshi Bhoyar <161412155+Sakshibhoyar16@users.noreply.github.com>
2026-06-18 15:37:59 +0530

    Remove arithmetic functions and add print statement

--- calculator.py
+++ calculator.py
@@ -1,17 +1,1 @@
-def add(a, b):
-    """Adds two numbers and returns the result."""
-    return a + b
-
-def subtract(a, b):
-    """Subtracts b from a and returns the result."""
-    return a - b
-
-def multiply(a, b):
-    """Multiplies two numbers."""
-    return a * b
-def divide(a,b):
-    """Divide two numbers."""
-    return a/b
-def cube(n):
-    """Returns the cube of a number."""
-    return n ** 3
+print("Hello World")
